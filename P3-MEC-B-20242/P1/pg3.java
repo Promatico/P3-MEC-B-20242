853138fa7b58285fa7af54798cee129795cd29e3
Promatico <rinconjuandavid761@gmail.com>
2024-08-27 15:53:07 -0500

    Update pg3.java

--- P3-MEC-B-20242/P1/pg3.java
+++ P3-MEC-B-20242/P1/pg3.java
@@ -1,12 +1,15 @@
-public class triangulo 
-private int altura 
-    private int base 
- 
-  public triangulo (int figura lt, int figura ly) {
-        if (figura == 0) {
 
 
- public static triangilorectangulo suma(figura  f1, figura  f2) {
+class TrianguloRectangulo:
+    def __init__(self, cateto1, cateto2):
+        self.cateto1 = cateto1
+        self.cateto2 = cateto2
 
-        int triangulo = lado 1 + lado 2 + lado 3;
-        int areatrinagulo  = figura 1 + figura 2;
+    def area(self):
+        # Método puro para calcular el área del triángulo rectángulo
+        return 0.5 * self.cateto1 * self.cateto2
+
+    def perimetro(self):
+        # Método puro para calcular el perímetro del triángulo rectángulo
+        hipotenusa = math.sqrt(self.cateto1**2 + self.cateto2**2)
+        return self.cateto1 + self.cateto2 
