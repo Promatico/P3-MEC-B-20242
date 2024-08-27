6621b64f441024b8399df352a10a3456e99194f0
Promatico <rinconjuandavid761@gmail.com>
2024-08-27 15:32:30 -0500

    Create pg2.java

---
+++ P3-MEC-B-20242/P1/pg2.java
@@ -0,0 +1,9 @@
+public class triangulo 
+private int altura 
+    private int base 
+ 
+  public triangulo (int figura lt, int figura ly) {
+        if (figura == 0) {
+
+ public triangulo 
+          
