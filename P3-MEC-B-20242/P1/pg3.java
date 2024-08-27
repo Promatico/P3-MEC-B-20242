4ad5517bd5479c3e72e468540c8789a61b03f1a4
Promatico <rinconjuandavid761@gmail.com>
2024-08-27 15:43:06 -0500

    Create pg3.java

---
+++ P3-MEC-B-20242/P1/pg3.java
@@ -0,0 +1,12 @@
+public class triangulo 
+private int altura 
+    private int base 
+ 
+  public triangulo (int figura lt, int figura ly) {
+        if (figura == 0) {
+
+
+ public static triangilorectangulo suma(figura  f1, figura  f2) {
+
+        int triangulo = lado 1 + lado 2 + lado 3;
+        int areatrinagulo  = figura 1 + figura 2;
